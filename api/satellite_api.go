33f967f6c90edad541e708fae0c1e92547d24717
Jhonathan Motta <jhonathan.motta@intraway.com>
2021-10-13 09:35:13 -0500

    Implementación para el  mensaje secreto

--- api/satellite_api.go
+++ api/satellite_api.go
@@ -38,6 +38,7 @@ func CreateSatellite(w http.ResponseWriter, r *http.Request) {
 	for index, element := range items.Satellite {
 
 		log.Println(index)
+
 		var con trilateration.Cordinate
 		con = trilateration.SateliteCordinate(element.Name, element.Distance)
 		cons = append(cons, con)
@@ -46,9 +47,23 @@ func CreateSatellite(w http.ResponseWriter, r *http.Request) {
 	x, y := cons[0].Trilaterate(cons[1], cons[2])
 	log.Println("Cordinates of intersections center using formula x:", x, "y:", y)
 
+	var secretMessage string
+
+	for i, a := range items.Satellite[0].Message {
+		if a != "" {
+			secretMessage = secretMessage + " " + a
+		} else if items.Satellite[1].Message[i] != "" {
+			secretMessage = secretMessage + " " + items.Satellite[1].Message[i]
+		} else {
+			secretMessage = secretMessage + " " + items.Satellite[2].Message[i]
+		}
+	}
+
 	var position dto.PositionDto
 	position.Position.X = x
 	position.Position.Y = y
+	position.Message = secretMessage
+
 	log.Println(cons)
 	json.NewEncoder(w).Encode(position)
 }
